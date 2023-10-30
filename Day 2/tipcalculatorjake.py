0aaab924feda7699622ad190033f3ecd06038a16
JakeyoloxD <148301003+JakeyoloxD@users.noreply.github.com>
2023-10-30 17:34:50 +1100

    Tip calc complete

--- Day 2/tipcalculatorjake.py
+++ Day 2/tipcalculatorjake.py
@@ -8,5 +8,11 @@
 #Write your code below this line 👇 
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give 10, 12, or 15? digits only" ))
-people = int(input("How many people to split the bill?"))
+tip = int(input("How much tip would you like to give 10, 12, or 15? digits only " ))
+people = int(input("How many people to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person sohould pay: ${final_amount}")
